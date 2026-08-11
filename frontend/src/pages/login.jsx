d005a0996b8411265178f424444be1e93e0f7603
francis <francisgemuelb@gmail.com>
2026-08-11 22:52:33 +0800

    Update login.jsx

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
 
         navigate("/dashboard");
     };
-    //redesign
+
     return (
         <div className="min-h-screen flex justify-center items-center">
             <form
