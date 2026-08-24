d8d41a1460cb76af2da4552c948dfddcd9fc13e4
francis <francisgemuelb@gmail.com>
2026-08-24 23:49:44 +0800

    Update supabase.js

--- frontend/src/services/supabase.js
+++ frontend/src/services/supabase.js
@@ -1,7 +1,7 @@
 // src/services/supabase.js
 
 import { createClient } from '@supabase/supabase-js';
-
+//create .env
 export const supabase = createClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_KEY
